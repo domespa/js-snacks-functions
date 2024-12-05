150a472f6a01181fe2f6c4e32cb2b8c2ef128281
domespa <dumiii1988@gmail.com>
2024-12-05 17:23:26 +0100

    feat: Filtra iniziali

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function getFirstLetterOfName(names, letter) {
+    const firstLetterOfName = [];
+    for (let i = 0; i < names.length; i++) {
+        if (names[i][0] === letter) {
+            firstLetterOfName.push(names[i])
+        }
+    }
+    return firstLetterOfName
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const searchForFirstLetter = getFirstLetterOfName(names, "A");
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+console.log(searchForFirstLetter)
