d4adecf8d787877fab9053335113682a2b8f6d25
domespa <dumiii1988@gmail.com>
2024-12-05 17:48:44 +0100

    feat: contavocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,24 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = 'EugEnio';
 
 // Dichiara la funzione qui.
 
+function searchVowels(word) {
+    const vowels = "aAeEiIoOuU";
+    const numOfVowels = [];
+    for (let i = 0; i < word.length; i++) {
+        if(vowels.includes(word[i])) {
+            numOfVowels.push(word[i]);
+        }
+    }
+    return numOfVowels
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const countVowels = searchVowels(word)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+console.log(countVowels)
