6ae66c751682d262c88af7a5d2e0ed806fb174e8
domespa <dumiii1988@gmail.com>
2024-12-05 18:01:59 +0100

    feat: saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -5,9 +5,15 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+function salut(userName) {
+    // return "Ciao" + " " + userName;
+    return `Ciao ${userName}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const autoSalut = salut(userName)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+console.log(autoSalut)
